fecdeaddec80dc7fb61cbf26f3ae26c8cb8b64e2
Jessica del cueto <jessicadelcueto@gmail.com>
2015-02-23 07:25:29 -0800

    modification fichier index page accueil

--- bucketshow/index.php
+++ bucketshow/index.php
@@ -1,3 +1,56 @@
 <?php get_header();  ?>
 
 <p class="text-uppercase">test</p>
+
+
+<section class="container">
+	<div class="carroussel"></div>
+</section>
+
+<section class="container">
+	<div class="row">
+
+		
+			<!--<h1 class="col-lg-offset-1 col-lg-10 col-sm-12">SPECTACLES À VENIR!</h1>
+			<hr class="col-lg-offset-1 col-lg-10 col-sm-12"></hr>-->
+			
+		<h1 class="col-lg-offset-1 col-lg-10 col-md-offset-1 col-md-10 col-sm-12">SPECTACLES À VENIR!</h1>
+		<hr class="col-lg-offset-1 col-lg-10 col-md-offset-1 col-md-10 col-sm-12">
+
+		<!--<div class="col-lg-offset-1 col-lg-5 col-sm-12 margin">-->
+		<div class="col-lg-offset-1 col-lg-5 col-md-offset-1 col-md-5 col-sm-12 margin">
+			<img src="<?php echo get_template_directory_uri() ?>/img/Louis-Jean-Cormier.jpg" alt="photo artiste"/>
+			<div class="bas">
+				<div class="infos">
+					<span class="jaune artiste">nom de l'artiste</span><br>
+					<span class="blanc">date</span><br>
+					<span class="blanc">$prix</span><br>
+				</div>				
+				
+				<div class="billet"></div>
+
+			</div>
+		</div>
+
+		<!--<div class="col-lg-5 col-sm-12 margin">-->
+		<div class="col-lg-5 col-md-5 col-sm-12 margin">
+			<img src="<?php echo get_template_directory_uri() ?>/img/Jesse-Cook.jpg" alt="photo artiste"/>
+			<div class="bas">
+				<div class="infos">
+					<span class="jaune artiste">nom de l'artiste</span><br>
+					<span class="blanc">date</span><br>
+					<span class="blanc">$prix</span><br>
+				</div>	
+				
+				<div class="billet"></div>
+
+			</div>
+		</div>
+
+
+	</div>
+</section>
+
+
+
+
